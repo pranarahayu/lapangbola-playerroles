133f116f5c168f40cd0c267fc90071f7591f2023
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-01-23 09:53:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ st.markdown('Created by: Prana - R&D Division Lapangbola.com')
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    r_filter = st.selectbox('Select Position', ['Center Back', 'Forward'])
+    s_filter = st.selectbox('Select Position', ['Center Back', 'Forward'])
 with col2:
     p_filter = st.selectbox('Select Player', ['Ilija Spasojevic', 'David da Silva'])
+with col3:
+    r_filter = st.selectbox('Select Role', ['Ball Playing Defender', 'Advanced Forward'])
