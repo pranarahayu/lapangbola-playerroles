2d84546345c292e923b577ad6b2fc4322746b242
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-01-24 14:49:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ else:
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM playerdb;', ttl="10m")
-st.write(df)
+df = conn.query('SELECT * FROM mytable;', ttl="10m")
 
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
