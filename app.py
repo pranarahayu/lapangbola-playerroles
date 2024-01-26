9cc6fe71c2be9f81ab1fadc2a70c28509cf7e148
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2024-01-26 10:24:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ st.set_page_config(page_title='Lapangbola - Player Roles', layout='wide')
 st.header('Lapangbola - Player Roles')
 st.markdown('Created by: Prana - R&D Division Lapangbola.com')
 
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    xlsx_url = sheets_url.replace("/edit#gid=", "/export?format=xlsx&gid=")
+    return pd.read_excel(xlsx_url)
+
 df = load_data(st.secrets["roles"])
 roles = load_data(st.secrets["rlz"])
 
